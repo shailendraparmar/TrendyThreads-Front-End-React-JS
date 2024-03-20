1b542c436403dcf6dbacc4fa7de6310277acf6fe
Yash <eldragoyash@gmail.com>
2024-03-20 18:15:43 +0530

    signup add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import SignUp from "./components/SignUp";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
+      <SignUp />
     </div>
   );
 }
